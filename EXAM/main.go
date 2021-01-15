e9c9fbf139e4ea4555b3016e14f625132bff9371
Brandon Mathurel <brandon.mathurel@gmail.com>
2021-01-15 17:07:58 +0100

    Rendu

--- EXAM/main.go
+++ EXAM/main.go
@@ -1,11 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"io"
+	"net/http"
+	"log"
+)
+
+
+type Task struct {
+        Description string
+       	Done bool
+}
+
+type List struct {
+	ID int
+	Task string
+}
+var task []Task
+
+func list(rw http.ResponseWriter, _ *http.Request) {
+        //io.WriteString(w, "Fonction List\n")
+	for i,val := range task{
+		if val.Done == false {
+			list = append(list, List{strconv.Itoa(id),val.Description})
+		}
+	rw.WriteHeader(http.StatusOK)
+
+}
+
+func done(rw http.ResponseWriter, _ *http.Request) {
+        io.WriteString(w, "Fonction Done\n")
+}
+
+func add(rw http.ResponseWriter, _ *http.Request) {
+        io.WriteString(w, "Fonction Add\n")
+}
 
 func main() {
-	type Task struct {
-		Description string
-		Done bool
-	}
 
+	http.HandleFunc("/", list)
+	http.HandleFunc("/done", done)
+	http.HandleFunc("/add", add)
+
+	log.Printf("About to listen on 8080, Go to http://127.0.0.0:8080/")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
